43aecde9d0e02920576de6933ae33b67d21adb84
BoyAnh <90632032+NguyenTheAnhDV19@users.noreply.github.com>
2023-01-10 21:36:33 +0700

    Add files via upload

---
+++ DHT11.cpp
@@ -0,0 +1,61 @@
+#include "DHT11.hpp"
+#include<Arduino.h>
+
+DHT11::DHT11(int pin)
+{
+    _pin = pin;
+}
+
+void DHT11::get_data(){
+  //init here
+  pinMode(_pin,OUTPUT);
+  digitalWrite(_pin,LOW);
+  delay(20);
+  pinMode(_pin,INPUT_PULLUP);
+  delayMicroseconds(55);
+
+  get_time(80,HIGH);
+  get_time(80,LOW);
+
+  //getbyte here
+  for(int i=0;i<5;i++){
+    for(int j=0;j<8;j++){
+      int time_50 =get_time(50,HIGH);
+      int time = get_time(70,LOW);
+      if(time< time_50){
+        (data[i] <<=1);
+      }else{
+        data[i] = (data[i] <<1) |1;
+      }
+    }
+  }
+  if(debug() == false) Serial.println("Error");
+}
+
+float DHT11::get_temperature(bool C){
+    if(C){
+        return data[2];
+    }else{
+        return (data[2]*9/5 +32);
+    }
+}
+
+int DHT11::get_humidity(){
+  return data[0];
+}
+
+
+int DHT11::get_time(int up,bool state){
+    uint16_t count = micros();
+    while(1){
+        if(digitalRead(_pin)== state){
+            return micros()-count;
+        }
+    }
+}
+
+bool DHT11::debug()
+{
+    if(data[0]+data[1]+data[2]+data[3] != data[4]) return false;
+    return true;
+}
---
+++ DHT11.hpp
@@ -0,0 +1,21 @@
+#ifndef _DHT11_
+#define _DHT11_
+#include<Arduino.h>
+#include<stdint.h>
+#include<stdio.h>
+
+class DHT11{
+private:
+  uint8_t data[5];
+  int _pin;
+  int get_time(int up, bool state);
+  bool debug();
+
+public:
+  DHT11(int pin);
+  void get_data();
+  float get_temperature(bool C);
+  int get_humidity();
+};
+
+#endif
