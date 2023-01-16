89ed97916637b2d335ac10e50305d1d3f07a68dc
BoyAnh <90632032+NguyenTheAnhDV19@users.noreply.github.com>
2023-01-16 21:18:32 +0700

    Update LCD.cpp

--- LCD.cpp
+++ LCD.cpp
@@ -1,6 +1,6 @@
 /* 
-It lacks a lot of feature though but i write it for fun :)))
-Note that this library is written based on LiquidCrystle_I2C
+It lacks a lot of feature though but i write it for fun so anyway :)))
+Note that this library was written based on LiquidCrystle_I2C library on GITHUB 
 The link to the datasheet of HD448780:
 	https://html.alldatasheet.com/html-pdf/63673/HITACHI/HD44780/11541/46/HD44780.html
 */
