6525bdbced3bf40acf0c62a85454c129d3fb1999
BoyAnh <90632032+NguyenTheAnhDV19@users.noreply.github.com>
2023-01-28 21:07:08 +0700

    Update Timer.cpp

--- Timer.cpp
+++ Timer.cpp
@@ -58,9 +58,20 @@ String Timer::convert(int number)
   }
 }
 
-void Timer::init()
+void Timer::init(String countTime)
 {
   time = millis()/1000;
+
+  hoursCount   = countTime.substring(0,2).toInt();
+  minutesCount = countTime.substring(3,5).toInt();
+  secondsCount = countTime.substring(6,8).toInt();
+
+  if(secondsCount == 0){
+     if(minutesCount == 0){
+        hoursCount--;
+     }
+     minutesCount--;
+  }
 }
 
 void Timer:: resetSeconds()
@@ -88,7 +99,7 @@ void Timer::resetAll()
 
 int Timer::second()
 {
-    return seconds;
+    return (int)seconds;
 }
 
 int Timer::minute()
@@ -101,3 +112,29 @@ int Timer::hour()
     return hours;
 }
 
+String Timer::countDown()
+{
+    getSeconds();
+    getMinutes();
+    getHours();
+
+    if(checkTimeOut() == 1) resetAll();
+
+    return convertCountDown(hoursCount, hours) + ':' + convertCountDown(minutesCount, minutes) + ':' + convertCountDown(secondsCount, seconds);
+
+}
+
+String Timer::convertCountDown(int time1, int time2)
+{
+    if(time1 - time2 <0){
+        return convert(60 -(time2 - time1));
+    }else{
+        return convert(time1 - time2);
+    }
+}
+
+bool Timer::checkTimeOut()
+{
+    if(hoursCount == hours && minutesCount == minutes && secondsCount == seconds) return true;
+    return false;
+}
